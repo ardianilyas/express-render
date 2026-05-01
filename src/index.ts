a07781d3dde8ef636704765006846b9a0bedc7b3
ardianilyas <ardianilyas@gmail.com>
2026-05-02 00:36:59 +0700

    push to repo

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (_req, res) => {
+  res.send("Hello from Express + TS 🚀");
+});
+
+app.get("/health", (_req, res) => {
+  res.status(200).send({ message: "OK" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
